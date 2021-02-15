3a0dc28447a0c2fe272d9c5dfa9c666e6bcd0b59
Dallen Pyrah <dallenpyrah@gmail.com>
2021-02-15 14:55:01 -0700

    finished

---
+++ app.js
@@ -0,0 +1,387 @@
+class Zoo{
+    constructor(cage, aquarium, openLand, house){
+        this.cage = cage;
+        this.aquarium = aquarium;
+        this.openLand = openLand;
+        this.house = house;
+    }
+}
+
+
+let zoo = new Zoo([],[],[],[])
+console.log(zoo.aquarium, zoo.cage, zoo.house, zoo.openLand)
+
+class Alligator{
+        constructor(name, color, gender, weight, height, teeth){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight; 
+        this.height = height;
+        this.teeth = teeth;
+    }
+}
+
+class Bat{
+    constructor(name, color, gender, weight, height, teeth, scary){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.teeth = teeth;
+        this.scary = scary;
+    }
+}
+
+
+
+class Cat{
+    constructor(name, color, gender, weight, height, teeth, mean){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.teeth = teeth;
+        this.mean = mean;
+    }
+}
+
+class Dog{
+    constructor(name, color, gender, weight, height, teeth, cute){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.teeth = teeth;
+        this.cute = cute;
+    }
+}
+
+class Elephant{
+    constructor(name, color, gender, weight, height, teeth, funny){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.teeth = teeth;
+        this.funny = funny;
+    }
+}
+
+class Fish{
+    constructor(name, color, gender, weight, length, teeth, swim){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.length = length;
+        this.teeth = teeth;
+        this.swim = swim;
+    }
+}
+
+class Gorilla{
+    constructor(name, color, gender, weight, height, teeth, hairy){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.teeth = teeth;
+        this.hairy = hairy;
+    }
+}
+
+class Human{
+    constructor(name, ethnicity, gender, weight, height, teeth, stupid){
+        this.name = name;
+        this.ethnicity = ethnicity;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.teeth = teeth;
+        this.stupid = stupid;
+    }
+}
+
+class ImmortalJellyfish{
+    constructor(name, color, gender, weight, length, mouth, hurts){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.length = length;
+        this.mouth = mouth;
+        this.hurts = hurts;
+    }
+}
+
+class Jaguar{
+    constructor(name, color, gender, weight, length, speed, bites){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.length = length;
+        this.speed = speed;
+        this.bites = bites;
+    }
+}
+
+class Kangaroo{
+    constructor(name, color, gender, weight, vertical, speed, kickPower){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.vertical = vertical;
+        this.speed = speed;
+        this.kickPower = kickPower;
+    }
+}
+
+class Lobster{
+    constructor(name, color, gender, weight, pinchPower, swims, jacked){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight; 
+        this.pinchPower = pinchPower;
+        this.swims = swims;
+        this.jacked = jacked;
+    }
+}
+
+class Mouse{
+    constructor(name, color, gender, weight, size, screams, cooks){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.size = size;
+        this.screams = screams;
+        this.cooks = cooks;
+    }
+}
+
+class Neanderthal{
+    constructor(name, ethnicity, gender, weight, speed, stupid, skills){
+        this.name = name;
+        this.ethnicity = ethnicity;
+        this.gender = gender;
+        this.weight = weight;
+        this.speed = speed;
+        this.stupid = stupid;
+        this.skills = skills;
+    }
+}
+
+class Octopus{
+    constructor(name, color, gender, length, tentecales, speed, hurts){
+        this.name = name;
+        this.color = color;
+        this.gender = gender; 
+        this.length = length;
+        this.tentecales = tentecales;
+        this.speed = speed;
+        this.hurts = hurts;
+    }
+}
+
+class Penguin{
+    constructor(name, color, gender, icedOut, vertical, pimp, children){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.icedOut = icedOut;
+        this.vertical = vertical;
+        this.pimp = pimp;
+        this.children = children;
+    }
+}
+
+class Quail{
+    constructor(name, color, gender, weight, dead, age){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.dead = dead;
+        this.age = age;
+    }
+}
+
+class Raccoon{
+    constructor(name, color, gender, weight, diet, friendly, rabies){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.diet = diet;
+        this.friendly = friendly;
+        this.rabies = rabies;
+    }
+}
+
+class Sloth{
+    constructor(name, color, gender, height, job, speed, friendly){
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.height = height;
+        this.job = job;
+        this.speed = speed;
+        this.friendly = friendly;
+    }
+}
+
+class Turkey{
+    constructor(name, hair, gender, weight, food, married, children){
+        this.name = name;
+        this.hair = hair;
+        this.gender = gender;
+        this.weight = weight;
+        this.food = food;
+        this.married = married;
+        this.children = children
+    }
+}
+
+class Uakari{
+    constructor(name, gender, species, weight, diet, swims, flys, rideOrDie){
+        this.name = name;
+        this.gender = gender;
+        this.species = species;
+        this.weight = weight;
+        this.diet = diet;
+        this.swims = swims;
+        this.flys = flys;
+        this.rideOrDie = rideOrDie;
+    }
+}
+
+class VampireSquid{
+    constructor(name, gender, fear, residence, friendly, bloodThirsty){
+        this.name = name; 
+        this.gender = gender;
+        this.fear = fear;
+        this.residence = residence;
+        this.friendly = friendly;
+        this.bloodThirsty = bloodThirsty;
+    }
+}
+
+class Walrus{
+    constructor(name, species, friendly, teeth, married, swims){
+        this.name = name;
+        this.species = species;
+        this.friendly = friendly;
+        this.teeth = teeth;
+        this.married = married;
+        this.swims = swims;
+    }
+}
+
+class Xerus{
+    constructor(name, gender, height, weight, walks, legs, hands){
+        this.name = name;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight; 
+        this.walks = walks;
+        this.legs = legs;
+        this.hands = hands;
+    }
+}
+
+class Zebra{
+    constructor(name, gender, height, weight, speed, stripes, home, freinds){
+        this.name = name;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.stripes = stripes;
+        this.home = home;
+        this.friends = freinds;
+    }
+}
+
+let animalAlligator = new Alligator("John", "Green","Male", 200, 10, 52);
+zoo.aquarium.push(animalAlligator);
+
+let animalBat = new Bat("Kayla","Black","Female", 3, 2, 12, false);
+zoo.cage.push(animalBat);
+
+let animalCat = new Cat("Luna", "White", "Female", 8, 1, 5000, true);
+zoo.house.push(animalCat);
+
+let animalDog = new Dog("Mozart", "White", "Male", 30, 3, 14, "Very Cute");
+zoo.house.push(animalDog);
+
+let animalElephant = new Elephant("Horton", "Gray", "Alien", 3000, 10, 2, true);
+zoo.openLand.push(animalElephant);
+
+let animalFish = new Fish("Nemo", "Orange", "Male", 1, 2, "None", false);
+zoo.aquarium.push(animalFish);
+
+let animalGorilla = new Gorilla("Harambe", "Black", "Male", 300, 5, 3, "Very Hairy");
+zoo.cage.push(animalGorilla);
+
+let animalHuman = new Human("Gibby", "White", "Male", 250, 6, 24, true);
+zoo.house.push(animalHuman);
+
+let animalImmortalJellyfish = new ImmortalJellyfish("Iris", "Transparent", "Female", 0, 37, false, "Hurts a lot");
+zoo.aquarium.push(animalImmortalJellyfish);
+
+let animalJaguar = new Jaguar("Yoda", "Yellow", "Female", 100, 5, 30, "Yes");
+zoo.openLand.push(animalJaguar);
+
+let animalKangaroo = new Kangaroo("Jerry", "Radiant Orange", "Male", 200, 48, "Slow", 9000);
+zoo.openLand.push(animalKangaroo);
+
+let animalLobster = new Lobster("Larry", "Red", "Male", 12, "Inmeasurable Pinch Power", "Barely Swims", "Absolutely Jacked");
+zoo.aquarium.push(animalLobster);
+
+let animalMouse = new Mouse("Ratata", "Brown", "None", 2, 1, "Sreams all the time", "Very good cook");
+zoo.house.push(animalMouse);
+
+let animalNeanderthal = new Neanderthal("Adam", "Canadian", "Male", 124, 18, "Very stupid", "Can build fires");
+zoo.cage.push(animalNeanderthal);
+
+let animalOctopus = new Octopus("Linus", "Bright Red", "Probably an alien", 30, 17, "Average Speed", "Don't get killed by him it hurts");
+zoo.aquarium.push(animalOctopus);
+
+let animalPenguin = new Penguin("Happy Feet", "Black and Yellow", "Undecided", "Very iced out", 89, true, 8);
+zoo.aquarium.push(animalPenguin);
+
+let animalQuail = new Quail("Caleb", "Black", "Female", 2, true, 18);
+zoo.cage.push(animalQuail);
+
+let animalRaccoon = new Raccoon("Loba", "Rustic Red", "Male", 32, "Will eat literally anything you give him", false, true);
+zoo.house.push(animalRaccoon);
+
+let animalSloth = new Sloth("Ray", "Brown", "Male", 8, "DMV Clerk", "Extremly Slow", true);
+zoo.cage.push(animalSloth);
+
+let animalTurkey = new Turkey("Gloria", "Bald", "Female", 23, "Eats other turkeys", false, 18);
+zoo.openLand.push(animalTurkey);
+
+let animalUakari = new Uakari("Boban", "Undecided", "Not sure what type of animal it is", 8919, "Eats humans alive", false, false, "Is a ride or die");
+zoo.cage.push(animalUakari);
+
+let animalVampireSquid = new VampireSquid("Squidward", "Male", "Spongebob and Patrick", "Lives in a totem", false, true);
+zoo.aquarium.push(animalVampireSquid);
+
+let animalWalrus = new Walrus("Yolanda", "A rare creature", "Will love you till it dies", 0, "Married for 37 years", false);
+zoo.aquarium.push(animalWalrus);
+
+let animalXerus = new Xerus("Rolondo", false, 3, 300, "Can't even walk", 3, 9);
+zoo.house.push(animalXerus);
+
+let animalZebra = new Zebra("Marty", "Male", 5, 600, "Very Fast", 18, "Madagascar", false);
+zoo.openLand.push(animalZebra);
